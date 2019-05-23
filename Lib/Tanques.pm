40c654c5e24dd0cc2c05891d509de8a447dad151
Carlos Gomez <cgomez@tecflo.com.mx>
2019-05-22 23:00:32 -0500

    obtener informacion de tanques a partir de ws

--- Lib/Tanques.pm
+++ Lib/Tanques.pm
@@ -79,13 +79,15 @@ sub getTanquesEstatus {
 	return @tanques;
 }
 
-sub getTanqueEstatus {
+sub getTanqueEstatusDeprecated {
 	my $tanque_id = shift;
 	my $tanque_name = shift;
 	my $tanque_capacity = shift;
 	my $wsc = Trate::Lib::WebServicesClient->new();
-	my $url = ORCUURL . "get_tls_wet_inventory.xml?ID=" . $wsc->sessionIdTransporter() . "&tank_id=" . $tanque_id . "&tank_name=" . $tanque_name;
+	my $url = "https://10.100.60.196/get_tls_wet_inventory.xml?ID=" . $wsc->sessionIdTransporter() . "&tank_id=" . $tanque_id . "&tank_name=" . $tanque_name;
+	LOGGER->debug($url);
 	my $contents = get($url);
+	LOGGER->debug($contents);
 	my %tanque = ();
 
 	my $dom = XML::LibXML->load_xml(string => $contents);
@@ -113,6 +115,53 @@ sub getTanqueEstatus {
 	}
 	return \%tanque;
 }
+
+
+sub getTanqueEstatus {
+	my $tanque_id = shift;
+	my $tanque_name = shift;
+	my $tanque_capacity = shift;
+	my $wsc = Trate::Lib::WebServicesClient->new();
+	my %tanque = ();
+
+	my %params = (
+		SessionID => "",
+		site_code => "",
+		tank_name => $tanque_name
+	);
+	my $wsc = Trate::Lib::WebServicesClient->new();
+	$wsc->callName("SOHOGetLastTLSReading");
+	$wsc->sessionIdTransporter();
+	my $result = $wsc->execute(\%params);
+	LOGGER->debug(dump($result));
+
+	%tanque = (
+		"name" => $tanque_name,
+		"tank_id" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{tank_oid},
+		"date" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{time},
+		"date_sql" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{time},
+		"fuel_height" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{height},
+		"water_height" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{water_height},
+		"fuel_volume" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{volume},
+		"water_volume" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{water_volume},
+		"temperature" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{temperature},
+		"tc_volume" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{tc_volume},
+		"density" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{density},
+		"density_15" => 0,
+		"tlh_id" => 0,
+		"user" => 0,
+		"probe_id" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{probe_oid},
+		"shift_id" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{shift_id},
+		"ullage" => $result->{a_soTankLevelHistory}->{soTankLevelHistory}->{ullage},
+		"capacity" => $tanque_capacity
+	);
+	return \%tanque;
+
+}
+
+
+
+
 
 1;
 #EOF
